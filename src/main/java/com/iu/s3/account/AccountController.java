0489be4697c2175162a596cb4650c1391d6cdf43
WonKyoSeo <swkgold@Gmail.com>
2021-03-26 02:53:47 +0900

    Initial commit

---
+++ src/main/java/com/iu/s3/account/AccountController.java
@@ -0,0 +1,13 @@
+package com.iu.s3.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account/**")
+public class AccountController {
+	
+	@RequestMapping("accountList")
+	public void getList()throws Exception{}
+
+}
---
+++ src/main/webapp/WEB-INF/views/account/accountList.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Account List Page</h1>
+	<h3>나는 아이유로 갈아탄다</h3>
+</body>
+</html>
