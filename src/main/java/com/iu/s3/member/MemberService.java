279b71c7d9c4c1f289aa590b5f2b627ba0722a26
WonKyoSeo <swkgold@Gmail.com>
2021-04-07 15:59:36 +0900

    학

--- src/main/java/com/iu/s3/member/MemberService.java
+++ src/main/java/com/iu/s3/member/MemberService.java
@@ -1,7 +1,15 @@
 package com.iu.s3.member;
 
+import java.io.File;
+import java.util.Calendar;
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class MemberService {
@@ -20,9 +28,26 @@ public class MemberService {
 	public MemberDTO memberLogin(MemberDTO memberDTO)throws Exception{
 		return memberDAO.memberLogin(memberDTO);
 	}
-	
-	public int memberJoin(MemberDTO memberDTO)throws Exception{
-		return memberDAO.memberJoin(memberDTO);
+												//이진데이터
+	public int memberJoin(MemberDTO memberDTO,MultipartFile avatar, HttpSession session)throws Exception{
+			//makePath()
+			
+			
+			
+			//5. HDD에 파일 저장
+			//file = new File(file, name);
+
+			//	1) FileCopyUtils 클래스의 copy 메서드 사용
+			//FileCopyUtils.copy(avatar.getBytes(), file);	       
+			
+			//	2) MultipartFile 클래스의 메서드 사용
+			file = new File(file,fileName);
+			avatar.transferTo(file);
+			
+			
+			
+		return 0;
+		//return memberDAO.memberJoin(memberDTO);
 	}
 
 }
