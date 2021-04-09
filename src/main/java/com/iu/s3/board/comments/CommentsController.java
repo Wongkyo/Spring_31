6ba3fa82e8492d42303247b28e36c7419deb4e41
WonKyoSeo <swkgold@Gmail.com>
2021-04-09 17:51:44 +0900

    sdfsd

--- src/main/java/com/iu/s3/board/comments/CommentsController.java
+++ src/main/java/com/iu/s3/board/comments/CommentsController.java
@@ -2,22 +2,37 @@ package com.iu.s3.board.comments;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/comments/**")
 public class CommentsController {
 	// /comments/commentsList
+	
+	@Autowired
 	private CommentsService commentsService;
 	
+	@PostMapping("commentsDelete")
+	public void commentsDelete(int [] commentNum)throws Exception{
+		System.out.println("Start");
+	
+		System.out.println("finish");
+	}
+	
+	
 	@PostMapping("commentsInsert")
-	public void setInsert(CommentsDTO commentsDTO)throws Exception{
-		System.out.println(commentsDTO.getWriter());
-		System.out.println(commentsDTO.getContents());
+	public ModelAndView setInsert(CommentsDTO commentsDTO)throws Exception{
+		ModelAndView mv = new ModelAndView();
+		int result =commentsService.setInsert(commentsDTO);
+		mv.addObject("result", result);
+		mv.setViewName("common/ajaxResult");
+		return mv;
 	}
 	
 	@GetMapping("commentsList")
